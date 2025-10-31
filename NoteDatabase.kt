a0d8adac7520dca2aaab266b0371a04d737c9a4e
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-31 16:19:05 +0100

    Update NoteDatabase.kt

--- NoteDatabase.kt
+++ NoteDatabase.kt
@@ -1,17 +1,16 @@
-package com.example.notesapp.data
+package com.example.smartnote.data
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Note::class], version = 1)
+@Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
     companion object {
-        @Volatile
-        private var INSTANCE: NoteDatabase? = null
+        @Volatile private var INSTANCE: NoteDatabase? = null
 
         fun getDatabase(context: Context): NoteDatabase {
             return INSTANCE ?: synchronized(this) {
