d47aba29e92ea2261e81a8350f14d08ace492d69
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-09 14:53:21 +0100

    Update Note.kt

--- Note.kt
+++ Note.kt
@@ -1,17 +1,14 @@
-package com.example.smartnoteapp.data
+package com.example.smartnote.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.text.SimpleDateFormat
-import java.util.*
 
 @Entity(tableName = "notes")
 data class Note(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val content: String,
-    val timestamp: String = SimpleDateFormat("dd MMM yyyy, hh:mm a", Locale.getDefault()).format(Date()),
+    val timestamp: String = "",
     val isPinned: Boolean = false,
     val isArchived: Boolean = false
 )
